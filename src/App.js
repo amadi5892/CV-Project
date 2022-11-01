1d029aa69ab76231c6b72c1d5273924e47981eed
amadi5892 <amadi5892@gmail.com>
2022-11-01 16:09:56 -0400

    added input with submit and edit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      submitted: false,
+    }
+  };
+
+  textEntered = (event) => {
+    const name = event.target.value;
+    this.setState(() => {
+      return { name };
+    })
+  };
+
+  onSubmit = () => {
+    console.log(this.state.submitted);
+    this.setState(() => {
+      if (!this.state.submitted) {
+        return {submitted: true};
+      }
+    })
+  };
+
+  onEdit = () => {
+    console.log(this.state.submitted);
+    this.setState(() => {
+      if(this.state.submitted) {
+        return {submitted: false};
+      }
+    })
+  };
+
+  
+  render () {
+    const {textEntered} = this;
+    const {onSubmit} = this;
+    const {onEdit} = this;
+    
+    
+    return (
+     <div>
+      <div>
+        {this.state.submitted ? <h1>{ this.state.name }</h1> : <p>Name: <input type='text' placeholder='Enter Name' onChange={textEntered} /></p>}
+      </div>
+      <div>
+        <button className="submit-button" onClick={onSubmit} >Submit</button>
+        <button className="edit-button" onClick={onEdit} >Edit</button>
+      </div>
+     </div>
+     
+    );
+  };
 }
 
 export default App;
